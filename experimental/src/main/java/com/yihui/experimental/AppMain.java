03cc57cf662a0c56f47a9596d1e17d37e9a26a21
yihuis <yihuis@amazon.com>
2022-03-29 17:45:56 -0700

    add codestyle for test

--- experimental/src/main/java/com/yihui/experimental/AppMain.java
+++ experimental/src/main/java/com/yihui/experimental/AppMain.java
@@ -1,2 +1,41 @@
-package com.yihui.experimental;public class AppMain {
+package com.yihui.experimental;
+
+import lombok.Data;
+
+import java.util.Optional;
+
+public class AppMain {
+
+    @Data
+    private static class Order {
+        private Customer customer;
+    }
+
+    @Data
+    private static class Customer {
+        private String customerId;
+    }
+
+    private static String myMethod(String s) {
+        System.out.println("myMethod is running");
+        return s + " is presented.";
+    }
+
+    private static void testOption(final Order order) {
+        String r = Optional.ofNullable(order)
+                .map(Order::getCustomer)
+                .map(Customer::getCustomerId)
+                .map(AppMain::myMethod)
+                .orElse("");
+
+        System.out.println("get result is: " + r);
+    }
+
+    public static void main(String[] args) {
+        final Order o = new Order();
+        final Customer c = new Customer();
+//        o.setCustomer(c);
+//        c.setCustomerId("ABC");
+        testOption(o);
+    }
 }
