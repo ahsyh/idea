12daeb44481fde048fe105ff7f7a6b73235b412b
yihuis <yihuis@amazon.com>
2022-03-31 15:59:55 -0700

    adjust score

--- tetris/src/main/java/com/yihui/tetris/controlpanel/UIContent.java
+++ tetris/src/main/java/com/yihui/tetris/controlpanel/UIContent.java
@@ -39,10 +39,10 @@ public final class UIContent {
         setBrickPositionY(MAX_BRICK_HEIGHT - current.getHeight());
     }
 
-    private static final int BONUS_SCORE_1 = 10;
-    private static final int BONUS_SCORE_2 = 30;
-    private static final int BONUS_SCORE_3 = 50;
-    private static final int BONUS_SCORE_4 = 80;
+    private static final int BONUS_SCORE_1 = 1;
+    private static final int BONUS_SCORE_2 = 3;
+    private static final int BONUS_SCORE_3 = 5;
+    private static final int BONUS_SCORE_4 = 8;
 
     private static final int LINE_NUMBER_1 = 1;
     private static final int LINE_NUMBER_2 = 2;
@@ -51,21 +51,22 @@ public final class UIContent {
 
     /**
      *
-     * @param cleanLines cleared line number
+     * @param cleanLines line
+     * @param speed speed
      */
-    public void updateScore(final int cleanLines) {
+    public void updateScore(final int cleanLines, final int speed) {
         switch (cleanLines) {
             case LINE_NUMBER_1:
-                score += BONUS_SCORE_1;
+                score += BONUS_SCORE_1 * speed;
                 break;
             case LINE_NUMBER_2:
-                score += BONUS_SCORE_2;
+                score += BONUS_SCORE_2 * speed;
                 break;
             case LINE_NUMBER_3:
-                score += BONUS_SCORE_3;
+                score += BONUS_SCORE_3 * speed;
                 break;
             case LINE_NUMBER_4:
-                score += BONUS_SCORE_4;
+                score += BONUS_SCORE_4 * speed;
                 break;
             default:
                 break;
