c3d1a91e50a9f5457e04379988533422f76354df
yihuis <yihuis@amazon.com>
2022-04-23 08:56:56 -0700

    use logger

--- f/src/main/java/com/yihui/f/AppMain.java
+++ f/src/main/java/com/yihui/f/AppMain.java
@@ -1,8 +1,16 @@
 package com.yihui.f;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static com.yihui.f.questions.Question001.test;
 
 public class AppMain {
+    /**
+     * Logger.
+     */
+    public static Logger logger = LoggerFactory.getLogger("f");
+
     public static void main(String[] args) {
         test();
     }
--- f/src/main/java/com/yihui/f/util/Log.java
+++ f/src/main/java/com/yihui/f/util/Log.java
@@ -1,10 +1,12 @@
 package com.yihui.f.util;
 
+import static com.yihui.f.AppMain.logger;
+
 public final class Log {
     public static void e(String msg) {
-        System.out.println(msg);
+        logger.warn(msg);
     }
     public static void i(String msg) {
-        //System.out.println(msg);
+        logger.info(msg);
     }
 }
